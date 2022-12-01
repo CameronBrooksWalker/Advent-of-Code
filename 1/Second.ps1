03469c1756237414dfa2b62dd4cb129e3a869a04
cameronbrookswalker <cameronbrookswalker@gmail.com>
2022-12-01 13:39:48 -0600

    Better Second solution

--- 1/Second.ps1
+++ 1/Second.ps1
@@ -9,8 +9,4 @@ if ($line) {$i += $line} else {$stock += $i ; $i = 0}
 
 }
 
-$snackers = $stock | sort -Descending | select -first 3
-
-foreach ($snacker in $snackers) {$total += $snacker}
-
-echo $total
+($stock | sort -Descending | select -first 3 | Measure-Object -Sum).Sum
