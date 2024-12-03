88fab789c684da73d38bfb044fb9ea6341f3970a
cameronbrookswalker <cameronbrookswalker@gmail.com>
2024-12-03 10:41:53 -0600

    regex continues to be evil

---
+++ 2024/3/second.ps1
@@ -0,0 +1,27 @@
+$timer =  [Diagnostics.Stopwatch]::StartNew()
+
+$input = Get-Content "C:\users\cwalker\Documents\AdventOfCode\Advent-of-Code\2024\3\input"
+
+$grabdodonts = "mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)"
+
+$grabints = '\d{1,3}'
+
+$isactive = $true
+
+foreach($value in [regex]::Matches($input, $grabdodonts).Value)
+{
+
+    if($value -like "do()"){$isactive = $true}
+    elseif($value -like "don't()"){$isactive = $false}
+    elseif($isactive)
+    {
+        [int[]]$curints = [regex]::Matches($value, $grabints).Value
+    
+        [int]$total += ($curints[0] * $curints[1])
+    }
+
+}
+
+write-host "total is :" $total -BackgroundColor Green
+
+Write-Host "total time was" $timer.elapsed "seconds"
