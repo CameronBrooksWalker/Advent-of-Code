9dc6fdaf2d1b763b54a477535836690494a2dec5
cameronbrookswalker <cameronbrookswalker@gmail.com>
2024-12-04 11:16:03 -0600

    Finished part one of day 4

--- 2024/4/first.ps1
+++ 2024/4/first.ps1
@@ -2,12 +2,9 @@ $timer =  [Diagnostics.Stopwatch]::StartNew()
 
 $global:xmastimes = 0
 
-
-
-
 function Main
 {
-    $infile = Get-Content "C:\users\cwalker\Documents\AdventOfCode\Advent-of-Code\2024\4\testinput"
+    $infile = Get-Content "C:\users\cwalker\Documents\AdventOfCode\Advent-of-Code\2024\4\input"
     $global:XMAX = $infile[0].Length
     $global:YMAX = $infile.Count
 
@@ -17,8 +14,7 @@ function Main
         {
             if($infile[$y][$x] -like "X")
             {
-                Check-Horiz -file $infile -x $x -y $y
-                Check-Vert -file $infile -x $x -y $y
+                Check-Directions -file $infile -x $x -y $y
             }
         }
     }
@@ -26,10 +22,7 @@ function Main
 
 }
 
-
-
-
-function Check-Horiz {
+function Check-Directions {
     param (
         [string[]] $file,
         [int] $x, $y
@@ -41,52 +34,25 @@ function Check-Horiz {
     #right check
     if($x -le ($global:xmax - 3) -and $file[$y][$x+1] -eq 'M' -and $file[$y][$x+2] -eq 'A' -and $file[$y][$x+3] -eq 'S' ) {$global:xmastimes++}
 
-}
-
-function Check-Vert {
-    param (
-        [string[]] $file,
-        [int] $x, $y
-        )
-
     #up check
     if($y -ge 3 -and $file[$y-1][$x] -eq 'M' -and $file[$y-2][$x] -eq 'A' -and $file[$y-3][$x] -eq 'S' ) {$global:xmastimes++}
 
     #down check
-    if($y -le ($global:ymax - 3) -and $file[$y+1][$x] -eq 'M' -and $file[$y+2][$x] -eq 'A' -and $file[$y+3][$x] -eq 'S' ) {$global:xmastimes++}
-    
-}
-
-function Check-Diag {
-    param (
-        [string[]] $file,
-        [int] $x, $y
-        )
-    
+    if($y -le ($global:ymax - 4) -and $file[$y+1][$x] -eq 'M' -and $file[$y+2][$x] -eq 'A' -and $file[$y+3][$x] -eq 'S' ) {$global:xmastimes++}
 
     #up left check
     if($y -ge 3 -and $x -ge 3 -and $file[$y-1][$x-1] -eq 'M' -and $file[$y-2][$x-2] -eq 'A' -and $file[$y-3][$x-3] -eq 'S') {$global:xmastimes++}
 
     #up right check
-    if($y -ge 3 -and $x -le ($global:xmax - 3) -and $file[$y-1][$x-1] -eq 'M' -and $file[$y-2][$x-2] -eq 'A' -and $file[$y-3][$x-3] -eq 'S') {$global:xmastimes++}
+    if($y -ge 3 -and $x -le ($global:xmax - 4) -and $file[$y-1][$x+1] -eq 'M' -and $file[$y-2][$x+2] -eq 'A' -and $file[$y-3][$x+3] -eq 'S') {$global:xmastimes++}
 
+    #down right check
+    if($y -le ($global:YMAX - 4) -and $x -le ($global:XMAX - 4) -and $file[$y+1][$x+1] -eq 'M' -and $file[$y+2][$x+2] -eq 'A' -and $file[$y+3][$x+3] -eq 'S') {$global:xmastimes++}
 
+    #down left check
+    if($y -le ($global:YMAX - 4) -and $x -ge 3 -and $file[$y+1][$x-1] -eq 'M' -and $file[$y+2][$x-2] -eq 'A' -and $file[$y+3][$x-3] -eq 'S') {$global:xmastimes++}
 }
 
+Main #runs main function... not strictly necessary I guess, just felt more organized in the moment
 
-
-
-
-
-
-
-
-
-
-
-Main
-
-
-
-
-$timer.stop(); Write-Host "Script ran in:" $timer.elapsed.milliseconds "milliseconds" -BackgroundColor Gray -ForegroundColor Red
+$timer.stop(); Write-Host "Script ran in:" $timer.elapsed.seconds "seconds or" $timer.elapsed.milliseconds "milliseconds" -BackgroundColor Gray -ForegroundColor Red
